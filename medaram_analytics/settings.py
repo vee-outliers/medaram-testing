c5072c883c3b22311f82d5973a413099c6d66a19
vee-outliers <veeresh@outliers.tech>
2024-02-14 14:42:58 +0530

    changes

--- medaram_analytics/settings.py
+++ medaram_analytics/settings.py
@@ -171,7 +171,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = 'staticfiles'
+STATIC_URL = "/static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+# STATIC_ROOT = 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
